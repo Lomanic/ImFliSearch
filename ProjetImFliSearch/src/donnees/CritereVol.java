3ab54d9f10bbcc4df2e29fe8f86cafca47d6a38e
davidjgomes <davidjgomes@fcaca3a7-1abf-453b-a0c8-fdce95c53e0a>
2012-12-17 15:36:08 +0000

    Classe Aeroport finalisé. Il reste à créer la méthode permettant de récupérer le nom complet de l'aéroport grâce au cod IATA. Modification de la classe Ville pour l'adapter à la classe Aeroport.

---
+++ ProjetImFliSearch/src/donnees/CritereVol.java
@@ -0,0 +1,60 @@
+﻿package donnees;
+
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import org.geonames.Toponym;
+import org.geonames.ToponymSearchCriteria;
+import org.geonames.ToponymSearchResult;
+import org.geonames.WebService;
+import org.geonames.utils.Distance;
+
+import constantes.Constantes;
+
+public class CritereVol {
+	
+	private Ville chVilleDepart;
+	private Ville chVilleArrivee;
+	private int chDistance;
+	private int chAllerRetour;
+	private int chNbAdulte;
+	private int chNbEnfant;
+	//private int chNbBebe;
+	private int chClasse;
+	private int chJourAller;
+	private int chMoisAller;
+	private int chAnneeAller;
+	private int chJourRetour;
+	private int chMoisRetour;
+	private int chAnneeRetour;
+
+	
+	public CritereVol (Ville parVilleDepart, Ville parVilleArrivee, int parDistance, int parAllerRetour,int parNbAdulte,int parNbEnfant/*, int parNbBebe*/,
+			int parClasse, int parJourAller, int parMoisAller, int parAnneeAller,int parJourRetour,int parMoisRetour, int parAnneeRetour)
+	{
+			chVilleDepart=parVilleDepart;
+			chVilleArrivee=parVilleArrivee;
+			chDistance=parDistance;
+			chAllerRetour=parAllerRetour;
+			chNbAdulte=parNbAdulte;
+			chNbEnfant=parNbEnfant;
+			//chNbBebe=parNbBebe;
+			chClasse=parClasse;
+			chJourAller=parJourAller;
+			chMoisAller=parMoisAller;
+			chAnneeAller=parAnneeAller;
+			chJourRetour=parJourRetour;
+			chMoisRetour=parMoisRetour;
+			chAnneeRetour=parAnneeRetour;
+		
+	}
+	
+	public String toString()
+	{
+		return "La ville de depart est "+chVilleDepart.getChNom()+" pour le "+Constantes.jours[chJourAller]+" "+Constantes.mois[chMoisAller]+
+		" "+Constantes.annees[chAnneeAller]+" et la ville d'arrivee est "+chVilleArrivee.getChNom()+". \n"+"La distance choisie est de "+chDistance+" km."+" Le vol est un aller avec "+
+		Constantes.nombres[chNbAdulte]+" adulte(s), "+Constantes.nombres[chNbEnfant]+" enfant(s)  "/*+Constantes.nombres[chNbBebe]+" bebe(s) en "*/+Constantes.classes[chClasse]+".";
+	}	
+
+}
