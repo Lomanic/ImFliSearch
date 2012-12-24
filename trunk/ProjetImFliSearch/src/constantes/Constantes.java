abca0ef35fd079c49b8438eea284833cfdaaccd2
imaster92 <imaster92@fcaca3a7-1abf-453b-a0c8-fdce95c53e0a>
2012-12-24 01:59:39 +0000

    Changement pour Ajout de la mise à jour des jours lors du changement de mois et d'annee dans les selections

--- trunk/ProjetImFliSearch/src/constantes/Constantes.java
+++ trunk/ProjetImFliSearch/src/constantes/Constantes.java
@@ -1,19 +1,15 @@
 ﻿package constantes;
 
-import donnees.CritereVol;
-
 public class Constantes {
 	
 	public static String[] nombres = {"0","1","2","3","4","5","6"};
 	
-	public static String[] classes = {"Classe Economique","Classe Affaires","Premiere Classe"};
+	public static String[] classes = {"Classe Economique","Classe Affaires","Première Classe"};
 	
-	public static String[] jours ={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15",
-		"16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
+	public static String[] joursDepart, joursRetour;
 	
-	public static String[] mois ={"janvier","fevrier","mars","avril","mai","juin","juillet","aout",
-		"septembre","octobre","novembre","decembre"};
+	public static String[] mois ={"janvier","février","mars","avril","mai","juin","juillet","août",
+		"septembre","octobre","novembre","décembre"};
 	
-	public static String[] annees={"2012","2013","2014","2015"};
-	
+	public static String[] annees;
 }
