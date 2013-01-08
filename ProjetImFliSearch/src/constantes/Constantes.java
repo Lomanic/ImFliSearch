14028d4c3a149990b35aafecb2182ec866c53813
davidjgomes <davidjgomes@fcaca3a7-1abf-453b-a0c8-fdce95c53e0a>
2013-01-08 20:12:42 +0000

    Ajout d'une JDialog permettant d'indiquer l'âge des enfants.

--- ProjetImFliSearch/src/constantes/Constantes.java
+++ ProjetImFliSearch/src/constantes/Constantes.java
@@ -2,7 +2,7 @@
 
 public class Constantes {
 	
-	public static String[] nombres = {"0","1","2","3","4","5","6"};
+	public static String[] nombres = {"0","1","2","3","4","5","6","7"};
 	
 
 	public static String[] classes = {"Classe économique","Classe économique premium","Classe Affaires","Première classe"};
@@ -15,4 +15,8 @@ public class Constantes {
 		"septembre","octobre","novembre","décembre"};
 	
 	public static String[] annees;
+	
+	public static String[] agesEnfants = {"< 2 ans (sur les genoux)","2 ans","3 ans","4 ans","5 ans","6 ans","7 ans","8 ans","9 ans","10 ans",
+		"11 ans","12 ans","13 ans","14 ans","15 ans","16 ans","17 ans"};
+	
 }
